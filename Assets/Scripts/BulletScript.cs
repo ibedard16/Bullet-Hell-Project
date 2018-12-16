c90303664635e51c52aa2fd4376b59b99a9edeb1
Isaac Bedard <brokenLogician@gmail.com>
2018-12-16 18:09:47 -0500

    Make bullet prefab; test spawn bullets

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletScript : MonoBehaviour {
+	public delegate void UpdatePos(BulletScript updateBullet);
+	public UpdatePos updatePos;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Time.deltaTime, 0, 0);
+		updatePos(this);
 	}
 }
---
+++ Assets/Scripts/SimpleEnemyScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemyScript : MonoBehaviour {
+	public BulletScript bullet;
+	public float direction;
+	public int cooldown;
+
+	// Use this for initialization
+	void Start () {
+		direction = 0;
+		cooldown = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cooldown == 0) {
+			for (int i = 0; i < 20; ++i) {
+				var spawnedBullet = Instantiate(bullet);
+				var velocity = new Vector3(Mathf.Cos(direction), Mathf.Sin(direction), 0);
+				spawnedBullet.updatePos = (BulletScript updatingBullet) => {
+					updatingBullet.transform.position +=  Time.deltaTime * velocity;
+				};
+				direction += 0.1f;
+			}
+			cooldown = 10;
+		}
+		cooldown -= 1;
+	}
+}
